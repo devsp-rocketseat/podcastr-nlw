987c53afd826dc4f710144c2c213bb5bd33764f0
Vitor DevSP <contatodevsp@gmail.com>
2021-05-19 12:05:20 -0300

    Consumindo API na Home - SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <div></div>
+import { GetStaticProps } from 'next'
+
+export default function Home(props) {
+  return (
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8, // 8 hours
+  }
 }
