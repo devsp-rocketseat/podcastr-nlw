2d50f06e9cb2e49b4de11463126f0038f3671b6e
Vitor DevSP <contatodevsp@gmail.com>
2021-05-20 23:57:38 -0300

    Criando tipagens

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,7 @@ export default class MyDocument extends Document {
           />
           <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         </Head>
+
         <body lang="pt-br">
           <Main />
           <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,35 @@
 import { GetStaticProps } from 'next'
 
-export default function Home(props) {
+interface EpisodeProps {
+  id: string
+  title: string
+  members: string
+  publishedAt: string
+  thumbnail: string
+  description: string
+  file: {
+    url: string
+    type: string
+    duration: number
+  }
+}
+
+interface HomeProps {
+  episodes: EpisodeProps[]
+}
+
+export default function Home({ episodes }: HomeProps) {
   return (
     <div>
       <h1>Index</h1>
-      <p>{JSON.stringify(props.episodes)}</p>
+      <p>{JSON.stringify(episodes)}</p>
     </div>
   )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
   const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
+  const data: EpisodeProps = await response.json()
 
   return {
     props: {
