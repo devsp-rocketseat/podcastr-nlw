082ea8fbacdb880143f9acdd2e558c1cc5a725ff
Vitor DevSP <contatodevsp@gmail.com>
2021-05-24 17:16:51 -0300

    feat(page home): criando o roteamentento no next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from 'next'
 import Image from 'next/image'
+import Link from 'next/link'
 import { format, parseISO } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 
@@ -31,7 +32,6 @@ type EpisodeProps = {
   publishedAt: string
   duration: number
   durationAsString: string
-  description: string
   url: string
 }
 
@@ -58,7 +58,9 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
               />
 
               <div className={styles.episodeDetails}>
-                <a href="#">{episode.title}</a>
+                <Link href={`/episodes/${episode.id}`} >
+                  <a >{episode.title}</a>
+                </Link>
                 <p>{episode.members}</p>
                 <span>{episode.publishedAt}</span>
                 <span>{episode.durationAsString}</span>
@@ -99,7 +101,9 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   />
                 </td>
                 <td>
-                  <a href="#">{episode.title}</a>
+                  <Link href={`/episodes/${episode.id}`} >
+                    <a >{episode.title}</a>
+                  </Link>
                 </td>
                 <td>{episode.members}</td>
                 <td style={{ width: 100 }}>{episode.publishedAt}</td>
@@ -136,7 +140,6 @@ export const getStaticProps: GetStaticProps = async () => {
       publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
       duration: episode.file.duration,
       durationAsString: convertDurationToTimeString(episode.file.duration),
-      description: episode.description,
       url: episode.file.url,
     }
   })
